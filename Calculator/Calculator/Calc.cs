e355879af6a031c18a7c6d45606a56c28de3aabd
Michael Valentin <mv@signifly.com>
2011-11-22 19:30:47 +0100

    Changed a minor bug in the model

--- Calculator/Calculator/Calc.cs
+++ Calculator/Calculator/Calc.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 
 namespace Calculator
 {
@@ -8,13 +9,15 @@ namespace Calculator
     /// </summary>
     public class Calc
     {
-        private double _result = 0;
+        private double _result;
         private Dictionary<string, double> storage = new Dictionary<string, double>();
 
-        public void Update(Operation opr, double val)
+        public Calc()
         {
+            Clear();
         }
 
+        [Pure]
         public double Result
         {
             get { return _result; }
@@ -25,33 +28,37 @@ namespace Calculator
             _result = 0;
         }
 
+        [Pure]
         public double Recall(string index)
         {
-            return storage[index];
+            Contract.Requires(index != null);
+            return storage.ContainsKey(index) ? storage[index] : 0.0;
         }
 
         public void Store(string index, double val)
         {
+            Contract.Requires(index != null);
             storage[index] = val;
         }
 
-        private void Add(double val)
+        public void Add(double val)
         {
             _result += val;
         }
 
-        private void Subtract(double val)
+        public void Subtract(double val)
         {
             _result -= val;
         }
 
-        private void Multiply(double val)
+        public void Multiply(double val)
         {
             _result *= val;
         }
 
-        private void Divide(double val)
+        public void Divide(double val)
         {
+            Contract.Requires(val != 0);
             _result = _result / val;
         }
     }
