a413aa16f07ffb41eede69a4d66316f03f79fa08
Michael Valentin <mv@signifly.com>
2011-11-22 21:26:58 +0100

    Added pex-tests of model (Calc) class

--- Calculator/Calculator/Calc.cs
+++ Calculator/Calculator/Calc.cs
@@ -43,22 +43,26 @@ namespace Calculator
 
         public void Add(double val)
         {
+            Contract.Ensures(_result == Contract.OldValue(_result) + val);
             _result += val;
         }
 
         public void Subtract(double val)
         {
+            Contract.Ensures(_result == Contract.OldValue(_result) - val);
             _result -= val;
         }
 
         public void Multiply(double val)
         {
+            Contract.Ensures(_result == Contract.OldValue(_result) * val);
             _result *= val;
         }
 
         public void Divide(double val)
         {
             Contract.Requires(val != 0);
+            Contract.Ensures(_result == Contract.OldValue(_result) / val);
             _result = _result / val;
         }
     }
