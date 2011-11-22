666bb9a63e6e93a4d92e34bb8df707c672f979b5
Michael Valentin <mv@signifly.com>
2011-11-22 20:55:49 +0100

    Added the missing start.cs file!

---
+++ Calculator/Calculator/Start.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Main.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Windows;
+
+namespace Calculator
+{
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Start
+    {
+        [System.STAThread]
+        public static void Main(string[] args)
+        {
+            Application app = new Application();
+            app.Startup += (object sender, StartupEventArgs e) =>
+            {
+                MainWindow view = new MainWindow();
+                view.Show();
+                Controler cont = new Controler(view, new Calc());
+            };
+            app.Run();
+        }
+    }
+}
